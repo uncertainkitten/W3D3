88181234370cbf32ff1414518900c8bd45b9d32d
Cassandra <uncertainkitten@gmail.com>
2018-09-05 13:50:01 -0700

    Associate relationships between Course, Enrollment, and User models

--- association_project/app/models/course.rb
+++ association_project/app/models/course.rb
@@ -11,4 +11,22 @@
 #
 
 class Course < ApplicationRecord
+  has_many :enrollments,
+  primary_key: :id,
+  foreign_key: :course_id,
+  class_name: :Enrollment
+  
+  has_many :enrolled_students,
+  through: :enrollments,
+  source: :student
+  
+  has_one :prerequisite,
+  primary_key: :id,
+  foreign_key: :prereq_id,
+  class_name: :Course
+  
+  belongs_to :instructor,
+  primary_key: :id,
+  foreign_key: :instructor_id,
+  class_name: :User
 end
